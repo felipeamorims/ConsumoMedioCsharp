048dad8c17858f53bbea2920bbabb5d69d053560
felipeamorims <133169503+felipeamorims@users.noreply.github.com>
2023-09-12 23:08:36 -0300

    Add files via upload

---
+++ Consumo/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Consumo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDistanciaKm = new System.Windows.Forms.TextBox();
+            this.txtConsumoLitros = new System.Windows.Forms.TextBox();
+            this.btnLimparTela = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lblConsumoAtual = new System.Windows.Forms.Label();
+            this.btnLimparArquivo = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Distância em Km:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Consumo em Litros:";
+            // 
+            // txtDistanciaKm
+            // 
+            this.txtDistanciaKm.Location = new System.Drawing.Point(107, 19);
+            this.txtDistanciaKm.Name = "txtDistanciaKm";
+            this.txtDistanciaKm.Size = new System.Drawing.Size(108, 20);
+            this.txtDistanciaKm.TabIndex = 5;
+            this.txtDistanciaKm.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDistanciaKm_KeyPress);
+            // 
+            // txtConsumoLitros
+            // 
+            this.txtConsumoLitros.Location = new System.Drawing.Point(117, 45);
+            this.txtConsumoLitros.Name = "txtConsumoLitros";
+            this.txtConsumoLitros.Size = new System.Drawing.Size(98, 20);
+            this.txtConsumoLitros.TabIndex = 6;
+            this.txtConsumoLitros.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtConsumoLitros_KeyPress);
+            // 
+            // btnLimparTela
+            // 
+            this.btnLimparTela.Location = new System.Drawing.Point(118, 137);
+            this.btnLimparTela.Name = "btnLimparTela";
+            this.btnLimparTela.Size = new System.Drawing.Size(97, 23);
+            this.btnLimparTela.TabIndex = 7;
+            this.btnLimparTela.Text = "Limpar Tela";
+            this.btnLimparTela.UseVisualStyleBackColor = true;
+            this.btnLimparTela.Click += new System.EventHandler(this.btnLimparTela_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(15, 71);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(97, 23);
+            this.btnCadastrar.TabIndex = 8;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // lblConsumoAtual
+            // 
+            this.lblConsumoAtual.AutoSize = true;
+            this.lblConsumoAtual.Location = new System.Drawing.Point(20, 112);
+            this.lblConsumoAtual.Name = "lblConsumoAtual";
+            this.lblConsumoAtual.Size = new System.Drawing.Size(81, 13);
+            this.lblConsumoAtual.TabIndex = 9;
+            this.lblConsumoAtual.Text = "Consumo Atual:";
+            // 
+            // btnLimparArquivo
+            // 
+            this.btnLimparArquivo.Location = new System.Drawing.Point(15, 137);
+            this.btnLimparArquivo.Name = "btnLimparArquivo";
+            this.btnLimparArquivo.Size = new System.Drawing.Size(97, 23);
+            this.btnLimparArquivo.TabIndex = 11;
+            this.btnLimparArquivo.Text = "Limpar Arquivo";
+            this.btnLimparArquivo.UseVisualStyleBackColor = true;
+            this.btnLimparArquivo.Click += new System.EventHandler(this.btnLimparArquivo_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(118, 71);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(97, 23);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 182);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimparArquivo);
+            this.Controls.Add(this.lblConsumoAtual);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.btnLimparTela);
+            this.Controls.Add(this.txtConsumoLitros);
+            this.Controls.Add(this.txtDistanciaKm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Consumo";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDistanciaKm;
+        private System.Windows.Forms.TextBox txtConsumoLitros;
+        private System.Windows.Forms.Button btnLimparTela;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label lblConsumoAtual;
+        private System.Windows.Forms.Button btnLimparArquivo;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Consumo/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Consumo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Função que não deixa digitar nada além de números e um único ponto
+        private void txtDistanciaKm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+            {
+                e.Handled = true;
+            }
+
+            //Apenas permite digitar um único ponto
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Função que não deixa digitar nada além de números e um único ponto
+        private void txtConsumoLitros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != ','))
+            {
+                e.Handled = true;
+            }
+
+            //Apenas permite digitar um único ponto
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Validação das textbox
+        public static bool verificaVazio(string texto_verificar)
+        {
+            if (texto_verificar == string.Empty) // Verifica se não digitou nada
+            {
+                return false;
+            }
+           
+            if (Convert.ToDouble(texto_verificar) == 0) // Verifica se digitou zero
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Função Cadastro
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            double distancia = 0, consumo = 0, resultado = 0;
+            
+
+            if (verificaVazio(txtDistanciaKm.Text) == false || verificaVazio(txtConsumoLitros.Text) == false)
+            {
+                MessageBox.Show("Dados inválidos, digite os dados novamente.", "Dados inválidos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                //Cálculo e mostrar resultado
+                distancia = Double.Parse(txtDistanciaKm.Text);
+                consumo = Double.Parse(txtConsumoLitros.Text);
+
+                resultado = distancia / consumo;
+
+                lblConsumoAtual.Text = "Consumo Atual: " + resultado.ToString("F1") + " Km/l"; // limitar numero de caracteres depois do ponto
+
+                //Cadastro no Arquivo de texto
+            
+                using (StreamWriter writer = new StreamWriter("cadastroConsumo.txt", true))
+                {
+                   writer.WriteLine(txtDistanciaKm.Text+" Km");
+                   writer.WriteLine(txtConsumoLitros.Text+" Litro(s)");
+                   writer.WriteLine(lblConsumoAtual.Text);
+                   writer.WriteLine("------------------------");
+                   MessageBox.Show("Cadastro realizado com sucesso!", "Cadastrar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } 
+            }
+        }
+
+        //Função Limpar Arquivo
+        private void btnLimparArquivo_Click(object sender, EventArgs e)
+        {            
+            string sourceFile = @"C:\Users\claud\source\repos\Consumo\Consumo\bin\Debug\cadastroConsumo.txt";
+
+            string[] lines;
+
+            if (File.Exists(sourceFile))
+            {
+                lines = File.ReadAllLines(sourceFile);
+
+                if (lines.Length == 0)
+                {
+                    MessageBox.Show("Seu arquivo está vazio!", "Limpar Arquivo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                else
+                {
+                    using (StreamWriter writer = File.CreateText(sourceFile))
+                    {
+                        writer.Write(string.Empty);
+                        writer.Close();
+                        MessageBox.Show("Arquivo de cadastro limpo com sucesso!", "Limpar Arquivo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Arquivo de cadastro inexistente! Gere o arquivo através da função 'Cadastrar'.", "Arquivo Inexistente", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        //Função Limpar Tela
+        private void btnLimparTela_Click(object sender, EventArgs e)
+        {
+            txtConsumoLitros.Text = string.Empty;
+            txtDistanciaKm.Text = string.Empty;
+            lblConsumoAtual.Text = "Consumo Atual:";
+
+        }
+
+        //Função Fechar Programa
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Até Breve!", "Finalizando...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
